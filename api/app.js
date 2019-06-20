2214543030c75def29a75e636a14bd5c4a66f7b9
admin <worldboss@mac.local>
2019-06-20 03:20:39 +0300

    init

---
+++ api/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Node API')
+})
+
+app.get('/getData', (req, res) => {
+    res.json({'message': 'Hello World'})
+})
+
+app.post('/postData', bodyParser.json(), (req, res) => {
+    res.json(req.body)
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
---
+++ src/app/root/root.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RootService {
+
+  constructor(private http: HttpClient) { }
+
+  getAPIData(){
+    return this.http.get('/api/getData')
+  }
+  postAPIData(){
+    return this.http.post('/api/postData', {data: 'test'})
+  }
+}
